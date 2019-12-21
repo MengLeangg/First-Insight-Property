9e535dc49f3a2fae496428720b6ae5b7fe5567d4
Mengleangg <mengleangtuong.it@gmail.com>
2019-12-21 16:03:10 +0700

    modify application form

---
+++ assets/js/App-form.js
@@ -0,0 +1,26 @@
+$(".participation").hide();
+$(".payment").hide();
+$("#participation-link").click(function(){
+  $(".company-detail").fadeOut(100);
+  $(".payment").fadeOut(100);
+  $(".participation").delay(100).fadeIn(100);
+  $("#company-link").removeClass("active");
+  $("#payment").removeClass("active");
+  $("#participation-link").addClass("active");
+});
+$("#company-link").click(function(){
+  $(".company-detail").delay(100).fadeIn(100);;
+  $(".participation").fadeOut(100);
+  $(".payment").fadeOut(100);
+  $("#company-link").addClass("active");
+  $("#participation-link").removeClass("active");
+  $("#payment-link").removeClass("active");
+});
+$("#payment-link").click(function(){
+    $(".payment").delay(100).fadeIn(100);;
+    $(".company-detail").fadeOut(100);
+    $(".participation").fadeOut(100);
+    $("#payment-link").addClass("active");
+    $("#company-link").removeClass("active");
+    $("#participation-link").removeClass("active");
+});
